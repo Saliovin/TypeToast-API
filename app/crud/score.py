08263050c7341840c72b35908ff00dc8dd1dfa3c
Saliovin <jonas.eukan@gmail.com>
2024-09-06 15:33:01 +0800

    Added weekly leaderboard get request

--- app/crud/score.py
+++ app/crud/score.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
 
 from app.models.score import Score
@@ -6,6 +7,19 @@ from app.schemas.score import ScoreCreate
 
 def get_scores(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Score).offset(skip).limit(limit).all()
+
+
+def get_weekly_leaderboard(db: Session):
+    dt = datetime.now().date()
+    start = dt - timedelta(days=dt.weekday())
+    end = start + timedelta(days=6)
+    return (
+        db.query(Score)
+        .filter(Score.datetime >= start)
+        .filter(Score.datetime <= end)
+        .limit(20)
+        .all()
+    )
 
 
 def create_score(db: Session, score: ScoreCreate):
--- app/routers/score.py
+++ app/routers/score.py
@@ -16,3 +16,9 @@ def create_score(score: schemas.ScoreCreate, db: Session = Depends(get_db)):
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     scores = crud.get_scores(db, skip=skip, limit=limit)
     return scores
+
+
+@router.get("/scores/weekly/", response_model=list[schemas.Score])
+def read_weekly_leaderboard(db: Session = Depends(get_db)):
+    scores = crud.get_weekly_leaderboard(db)
+    return scores
